35e64438bde5a62d44a6df795c981f5257cd70c4
Piotr Komorowski <komorowski.piotr.91@gmail.com>
2019-02-04 20:18:51 +0100

    Updates

---
+++ precache-manifest.4fae3c7ac2d0cdea8d9bca91624df30f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "4eda80947981ce70af601e5110eaa0f1",
+    "url": "/knives/static/media/product.4eda8094.jpeg"
+  },
+  {
+    "revision": "d180e1a18f7ac6e38122711b7a7215dc",
+    "url": "/knives/static/media/home.d180e1a1.jpeg"
+  },
+  {
+    "revision": "9ce2a0271f4ca3cedb40712576b35b7e",
+    "url": "/knives/static/media/banner.9ce2a027.jpg"
+  },
+  {
+    "revision": "6eac9fe7618d9a510074b8e8e05f7afb",
+    "url": "/knives/static/media/about.6eac9fe7.jpg"
+  },
+  {
+    "revision": "e25b3409968f174f0c50",
+    "url": "/knives/static/js/runtime~main.e25b3409.js"
+  },
+  {
+    "revision": "988fb8bd2fc047ae3b1a",
+    "url": "/knives/static/js/main.988fb8bd.chunk.js"
+  },
+  {
+    "revision": "b46b8484480ce9a9c3f4",
+    "url": "/knives/static/js/1.b46b8484.chunk.js"
+  },
+  {
+    "revision": "988fb8bd2fc047ae3b1a",
+    "url": "/knives/static/css/main.e959ebef.chunk.css"
+  },
+  {
+    "revision": "5073b9635f3d526305ba0840c1fd3bb8",
+    "url": "/knives/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/knives/precache-manifest.4fae3c7ac2d0cdea8d9bca91624df30f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/knives/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
